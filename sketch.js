8cccf08e581e36e2d2140989b47ad0056a9c39f0
Vilmaam18 <00001120552795sp@al.educacao.sp.gov.br>
2024-06-03 11:46:57 -0300

    adicionando arquivos js e cs

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background("red");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("green");
+  
+    //  console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+
